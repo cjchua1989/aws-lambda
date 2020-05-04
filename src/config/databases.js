b6e309293400f87d7ca8d68679239e0d68f4d4f4
CJ Chua <cjchua1989@gmail.com>
2020-05-04 19:00:39 +0800

    Added the database connection thru sequelize

--- src/config/databases.js
+++ src/config/databases.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+
 const connections = {
     main: {
         host: process.env.DB_HOST,
@@ -5,10 +7,44 @@ const connections = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
     },
 };
 
-let active = {};
+const active = {};
 
-const
+const getConnection = async (conn = 'main') => {
+    if (!active[conn]) {
+        const config = connections[conn];
+
+        active[conn] = new Sequelize(
+            config.database,
+            config.username,
+            config.password,
+            {
+                host: config.host,
+                port: config.port,
+                dialect: config.dialect,
+            }
+        );
+    }
+
+    return active[conn];
+};
+
+const closeConnection = async () => {
+    const closing = [];
+    const forClosing = active.values();
+
+    for (let i = 0; i < forClosing.length; i += 1) {
+        const connection = forClosing[i];
+        closing.push(connection.close());
+    }
+
+    await Promise.all(closing);
+};
+
+module.exports = {
+    getConnection,
+    closeConnection,
+};
